01433760c3dd889e9878e4ed0ee653948061b8c1
Bence Bánsághi <35151749+bencebansaghi@users.noreply.github.com>
2024-06-06 17:35:29 +0300

    trying to make it work

--- week1/index.js
+++ week1/index.js
@@ -6,17 +6,17 @@ function initializeCode() {
     const logHello = document.getElementById("my-button");
 
     logHello.addEventListener("click", function() {
-        console.log("Hello world!");
+        console.log("hello world");
 
         const h1Element = document.getElementsByTagName("h1")[0];
-        h1Element.textContent = "Moi maailma!";
+        h1Element.textContent = "Moi maailma";
     })
 
     const addListElement = document.getElementById("add-data");
     
     addListElement.addEventListener("click", function() {
         const list = document.getElementById("my-list");
-        const value = document.getElementById("text-input").value;
+        const value = document.getElementById("textarea").value;
         const listElement = document.createElement("li");
         listElement.textContent = value;
         list.appendChild(listElement);
