398912e5d53c8bdb8d12c2bdbe20fdede57100dc
Bence Bánsághi <35151749+bencebansaghi@users.noreply.github.com>
2024-06-06 17:26:45 +0300

    week1

---
+++ week1/index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    initializeCode();
+})
+
+function initializeCode() {
+    const logHello = document.getElementById("my-button");
+
+    logHello.addEventListener("click", function() {
+        console.log("Hello world!");
+
+        const h1Element = document.getElementsByTagName("h1")[0];
+        h1Element.textContent = "Moi maailma!";
+    })
+
+    const addListElement = document.getElementById("add-data");
+    
+    addListElement.addEventListener("click", function() {
+        const list = document.getElementById("my-list");
+        const value = document.getElementById("text-input").value;
+        const listElement = document.createElement("li");
+        listElement.textContent = value;
+        list.appendChild(listElement);
+    })
+
+
+
+}
